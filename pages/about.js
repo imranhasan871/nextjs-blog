b59f4e0d4a880e34f6b7267d482f956663ccb106
Imran Hasan <imranhasan871@gmail.com>
2023-02-01 17:30:25 +0600

    initial Commit

---
+++ pages/about.js
@@ -0,0 +1,20 @@
+export default function About() {
+	return (
+		// nav Section with Styles class from index.js
+		<nav>
+			<ul>
+				<li>
+					<a href='/'>Home</a>
+				</li>
+				<li>
+					<a href='/about'>About</a>
+				</li>
+				<li>
+					<a href='/contact'>Contact</a>
+				</li>
+			</ul>
+		</nav>
+	);
+}
+
+//
